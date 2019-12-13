658c01413277b072bd53442d225c83b10644af72
vivekv95 <vivekv210@gmail.com>
2019-12-13 17:31:20 -0600

    Initialize project with package structure

---
+++ student_art_collection/android/app/src/main/kotlin/com/lambdaschool/student_art_collection/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.lambdaschool.student_art_collection
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
