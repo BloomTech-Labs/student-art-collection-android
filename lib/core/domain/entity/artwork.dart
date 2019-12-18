9146ff71a5254ba1af19970325e75a47bffd3905
vivekv95 <vivekv210@gmail.com>
2019-12-18 12:45:44 -0600

    Create Artwork entity with nested Category and Images

---
+++ lib/core/domain/entity/artwork.dart
@@ -0,0 +1,73 @@
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+class Artwork extends Equatable {
+  final int artId;
+  final String title;
+  final Category category;
+  final double price;
+  final String artistName;
+  final bool sold;
+  final int schoolId;
+  final String description;
+  final DateTime datePosted;
+  final Images images;
+
+  Artwork({
+    @required this.artId,
+    this.title,
+    this.category,
+    @required this.price,
+    this.artistName,
+    this.sold,
+    @required this.schoolId,
+    this.description,
+    this.datePosted,
+    @required this.images,
+  });
+
+  @override
+  List<Object> get props => [
+        artId,
+        title,
+        category,
+        price,
+        artistName,
+        sold,
+        schoolId,
+        description,
+        datePosted,
+        images,
+      ];
+}
+
+class Category extends Equatable {
+  final int categoryId;
+  final String categoryName;
+
+  Category({
+    @required this.categoryId,
+    @required this.categoryName,
+  });
+
+  @override
+  List<Object> get props => [
+        categoryId,
+        categoryName,
+      ];
+}
+
+class Images extends Equatable {
+  final int imageId;
+  final int artId;
+  final String imageUrl;
+
+  Images({
+    @required this.imageId,
+    @required this.artId,
+    @required this.imageUrl,
+  });
+
+  @override
+  List<Object> get props => [imageId, artId, imageUrl];
+}
