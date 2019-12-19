49e3419472e3cfc0a99511d5f5714c52045fc879
vivekv95 <vivekv210@gmail.com>
2019-12-19 14:03:16 -0600

    Create base SchoolModel
    Co-authored-by: Brandon Lively <BrandonLively@github.com>
    Co-authored-by: Vivek Vishwanath <VivekV95@github.com>

---
+++ lib/core/data/model/image_model.dart
@@ -0,0 +1,19 @@
+import 'package:student_art_collection/core/domain/entity/artwork.dart';
+import 'package:meta/meta.dart';
+
+class ImageModel extends Image {
+  ImageModel({@required int artId, @required int imageId, String imageUrl})
+      : super(artId: artId, imageId: imageId, imageUrl: imageUrl);
+
+  factory ImageModel.fromJson(Map<String, dynamic> json) {
+    return ImageModel(
+      imageId: json['image_id'],
+      artId: json['art_id'],
+      imageUrl: json['image_url'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'image_id': imageId, 'art_id': artId, 'image_url': imageUrl};
+  }
+}
