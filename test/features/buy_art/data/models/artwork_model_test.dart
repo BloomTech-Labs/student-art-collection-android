21d3febc824e43e88c9873d15d266918b0f47c94
BrandonLively <brandonlively@live.com>
2019-12-19 15:33:01 -0500

    Refactored Models

--- test/features/buy_art/data/models/artwork_model_test.dart
+++ test/features/buy_art/data/models/artwork_model_test.dart
@@ -1,32 +1,31 @@
 import 'dart:convert';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:student_art_collection/core/data/model/image_model.dart';
-import 'package:student_art_collection/core/domain/entity/artwork.dart';
 import 'package:student_art_collection/core/data/model/artwork_model.dart';
 import 'package:student_art_collection/core/data/model/category_model.dart';
+import 'package:student_art_collection/core/data/model/image_model.dart';
+import 'package:student_art_collection/core/domain/entity/artwork.dart';
 
 import '../../../../core/fixtures/fixture_reader.dart';
 
 void main() {
-  final tImageList = [
-    ImageModel(imageId: 1, imageUrl: 'https://picsum.photos/200/300', artId: 1)
-  ];
+  final tImageList = [ImageModel(
+      imageId: 1, imageUrl: 'https://picsum.photos/200/300', artId: 1
+  )];
 
   final tCategory = CategoryModel(
-    categoryId: 1,
-    categoryName: 'test',
-  );
+    categoryId: 1, categoryName: 'test',);
 
   final tArtworkModel = ArtworkModel(
-      artId: 1,
-      artistName: "name",
-      category: tCategory,
-      images: tImageList,
-      price: 25.50,
-      schoolId: 1,
-      sold: false,
-      description: "description",
-      title: "title");
+    artId: 1,
+    artistName: "name",
+    category: tCategory,
+    images: tImageList,
+    price: 25.50,
+    schoolId: 1,
+    sold: false,
+    description: "description",
+    title: "title"
+  );
 
   test('should be a subclass of Artwork entity', () async {
     //assert
@@ -48,7 +47,10 @@ void main() {
       final result = tArtworkModel.toJson();
       final expectedMap = {
         "title": "title",
-        "category": {"category_id": 1, "category_name": "test"},
+        "category": {
+          "category_id": 1,
+          "category_name": "test"
+        },
         "price": 25.50,
         "artist_name": "name",
         "sold": false,
---
+++ test/features/buy_art/domain/usecase/get_artwork_by_id.dart
@@ -0,0 +1,33 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:student_art_collection/core/domain/entity/artwork.dart';
+import 'package:student_art_collection/features/buy_art/domain/repository/artwork_repository.dart';
+import 'package:student_art_collection/features/buy_art/domain/usecase/get_artwork_by_id.dart';
+
+class MockArtworkRepository extends Mock implements ArtworkRepository {}
+
+void main() {
+  GetArtworkByID usecase;
+  MockArtworkRepository mockArtworkRepository;
+
+  setUp(() {
+    mockArtworkRepository = MockArtworkRepository();
+    usecase = GetArtworkByID(mockArtworkRepository);
+  });
+  final tIDNumber = 1;
+  final tArtwork = Artwork(artId: 1, price: 25, schoolId: 2, images: [
+    Image(artId: 1, imageId: 1, imageUrl: 'https://picsum.photos/200/300')
+  ]);
+
+  test('should get artwork matching the id from the repository', () async {
+    //arrange
+    when(mockArtworkRepository.getArtworkById(any)).thenAnswer((_) async => Right(tArtwork));
+    //act
+    final result = await usecase(tIDNumber);
+    //assert
+    expect(result, Right(tArtwork));
+    verify(mockArtworkRepository.getArtworkById(tIDNumber));
+    verifyNoMoreInteractions(mockArtworkRepository);
+  });
+}
