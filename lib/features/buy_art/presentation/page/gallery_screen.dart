81236ea0e01eabd68b8a7a596b930326d47e0c10
BrandonLively <brandonlively@live.com>
2020-01-06 17:11:45 -0500

    Refactored Gallery Screen, Added loading indicator and border

--- lib/features/buy_art/presentation/page/gallery_screen.dart
+++ lib/features/buy_art/presentation/page/gallery_screen.dart
@@ -9,7 +9,6 @@ import 'package:student_art_collection/core/util/theme_constants.dart';
 class GalleryScreen extends StatefulWidget {
   static const ID = '/gallery';
 
-
   @override
   _GalleryScreenState createState() => _GalleryScreenState();
 }
@@ -18,78 +17,219 @@ class _GalleryScreenState extends State<GalleryScreen> {
   @override
   void initState() {
     super.initState();
-
   }
-
 
   @override
   Widget build(BuildContext context) {
     List<aw.Artwork> mockArtwork = [
-      aw.Artwork(artId: 1, images: [
-        aw.Image(imageId: 1, artId: 1, imageUrl: 'https://picsum.photos/600/900'),
-        aw.Image(imageId: 2, artId: 1, imageUrl: 'https://picsum.photos/600/900'),
-        aw.Image(imageId: 3, artId: 1, imageUrl: 'https://picsum.photos/600/900')],
-          price: 25, schoolId: 1 ),
-      aw.Artwork(artId: 1, images: [
-        aw.Image(imageId: 1, artId: 1, imageUrl: 'https://picsum.photos/601/900'),
-        aw.Image(imageId: 2, artId: 1, imageUrl: 'https://picsum.photos/600/900'),
-        aw.Image(imageId: 3, artId: 1, imageUrl: 'https://picsum.photos/600/900')],
-          price: 25, schoolId: 1 ),
-      aw.Artwork(artId: 1, images: [
-        aw.Image(imageId: 1, artId: 1, imageUrl: 'https://picsum.photos/602/900'),
-        aw.Image(imageId: 2, artId: 1, imageUrl: 'https://picsum.photos/600/900'),
-        aw.Image(imageId: 3, artId: 1, imageUrl: 'https://picsum.photos/600/900')],
-          price: 25, schoolId: 1 ),
-      aw.Artwork(artId: 1, images: [
-        aw.Image(imageId: 1, artId: 1, imageUrl: 'https://picsum.photos/603/900'),
-        aw.Image(imageId: 2, artId: 1, imageUrl: 'https://picsum.photos/600/900'),
-        aw.Image(imageId: 3, artId: 1, imageUrl: 'https://picsum.photos/600/900')],
-          price: 25, schoolId: 1 ),
-      aw.Artwork(artId: 1, images: [
-        aw.Image(imageId: 1, artId: 1, imageUrl: 'https://picsum.photos/604/900'),
-        aw.Image(imageId: 2, artId: 1, imageUrl: 'https://picsum.photos/600/900'),
-        aw.Image(imageId: 3, artId: 1, imageUrl: 'https://picsum.photos/600/900')],
-          price: 25, schoolId: 1 ),
-      aw.Artwork(artId: 1, images: [
-        aw.Image(imageId: 1, artId: 1, imageUrl: 'https://picsum.photos/605/900'),
-        aw.Image(imageId: 2, artId: 1, imageUrl: 'https://picsum.photos/600/900'),
-        aw.Image(imageId: 3, artId: 1, imageUrl: 'https://picsum.photos/600/900')],
-          price: 25, schoolId: 1 ),
-      aw.Artwork(artId: 1, images: [
-        aw.Image(imageId: 1, artId: 1, imageUrl: 'https://picsum.photos/606/900'),
-        aw.Image(imageId: 2, artId: 1, imageUrl: 'https://picsum.photos/600/900'),
-        aw.Image(imageId: 3, artId: 1, imageUrl: 'https://picsum.photos/600/900')],
-          price: 25, schoolId: 1 ),
-      aw.Artwork(artId: 1, images: [
-        aw.Image(imageId: 1, artId: 1, imageUrl: 'https://picsum.photos/607/900'),
-        aw.Image(imageId: 2, artId: 1, imageUrl: 'https://picsum.photos/600/900'),
-        aw.Image(imageId: 3, artId: 1, imageUrl: 'https://picsum.photos/600/900')],
-          price: 25, schoolId: 1 ),
-      aw.Artwork(artId: 1, images: [
-        aw.Image(imageId: 1, artId: 1, imageUrl: 'https://picsum.photos/608/900'),
-        aw.Image(imageId: 2, artId: 1, imageUrl: 'https://picsum.photos/600/900'),
-        aw.Image(imageId: 3, artId: 1, imageUrl: 'https://picsum.photos/600/900')],
-          price: 25, schoolId: 1 ),
-      aw.Artwork(artId: 1, images: [
-        aw.Image(imageId: 1, artId: 1, imageUrl: 'https://picsum.photos/609/900'),
-        aw.Image(imageId: 2, artId: 1, imageUrl: 'https://picsum.photos/600/900'),
-        aw.Image(imageId: 3, artId: 1, imageUrl: 'https://picsum.photos/600/900')],
-          price: 25, schoolId: 1 ),
-      aw.Artwork(artId: 1, images: [
-        aw.Image(imageId: 1, artId: 1, imageUrl: 'https://picsum.photos/610/900'),
-        aw.Image(imageId: 2, artId: 1, imageUrl: 'https://picsum.photos/600/900'),
-        aw.Image(imageId: 3, artId: 1, imageUrl: 'https://picsum.photos/600/900')],
-          price: 25, schoolId: 1 ),
-      aw.Artwork(artId: 1, images: [
-        aw.Image(imageId: 1, artId: 1, imageUrl: 'https://picsum.photos/611/900'),
-        aw.Image(imageId: 2, artId: 1, imageUrl: 'https://picsum.photos/600/900'),
-        aw.Image(imageId: 3, artId: 1, imageUrl: 'https://picsum.photos/600/900')],
-          price: 25, schoolId: 1 ),
-      aw.Artwork(artId: 1, images: [
-        aw.Image(imageId: 1, artId: 1, imageUrl: 'https://picsum.photos/612/900'),
-        aw.Image(imageId: 2, artId: 1, imageUrl: 'https://picsum.photos/600/900'),
-        aw.Image(imageId: 3, artId: 1, imageUrl: 'https://picsum.photos/600/900')],
-          price: 25, schoolId: 1 ),
+      aw.Artwork(
+          artId: 1,
+          images: [
+            aw.Image(
+                imageId: 1,
+                artId: 1,
+                imageUrl: 'https://picsum.photos/600/900'),
+            aw.Image(
+                imageId: 2,
+                artId: 1,
+                imageUrl: 'https://picsum.photos/600/900'),
+            aw.Image(
+                imageId: 3, artId: 1, imageUrl: 'https://picsum.photos/600/900')
+          ],
+          price: 25,
+          schoolId: 1),
+      aw.Artwork(
+          artId: 1,
+          images: [
+            aw.Image(
+                imageId: 1,
+                artId: 1,
+                imageUrl: 'https://picsum.photos/601/900'),
+            aw.Image(
+                imageId: 2,
+                artId: 1,
+                imageUrl: 'https://picsum.photos/600/900'),
+            aw.Image(
+                imageId: 3, artId: 1, imageUrl: 'https://picsum.photos/600/900')
+          ],
+          price: 25,
+          schoolId: 1),
+      aw.Artwork(
+          artId: 1,
+          images: [
+            aw.Image(
+                imageId: 1,
+                artId: 1,
+                imageUrl: 'https://picsum.photos/602/900'),
+            aw.Image(
+                imageId: 2,
+                artId: 1,
+                imageUrl: 'https://picsum.photos/600/900'),
+            aw.Image(
+                imageId: 3, artId: 1, imageUrl: 'https://picsum.photos/600/900')
+          ],
+          price: 25,
+          schoolId: 1),
+      aw.Artwork(
+          artId: 1,
+          images: [
+            aw.Image(
+                imageId: 1,
+                artId: 1,
+                imageUrl: 'https://picsum.photos/603/900'),
+            aw.Image(
+                imageId: 2,
+                artId: 1,
+                imageUrl: 'https://picsum.photos/600/900'),
+            aw.Image(
+                imageId: 3, artId: 1, imageUrl: 'https://picsum.photos/600/900')
+          ],
+          price: 25,
+          schoolId: 1),
+      aw.Artwork(
+          artId: 1,
+          images: [
+            aw.Image(
+                imageId: 1,
+                artId: 1,
+                imageUrl: 'https://picsum.photos/604/900'),
+            aw.Image(
+                imageId: 2,
+                artId: 1,
+                imageUrl: 'https://picsum.photos/600/900'),
+            aw.Image(
+                imageId: 3, artId: 1, imageUrl: 'https://picsum.photos/600/900')
+          ],
+          price: 25,
+          schoolId: 1),
+      aw.Artwork(
+          artId: 1,
+          images: [
+            aw.Image(
+                imageId: 1,
+                artId: 1,
+                imageUrl: 'https://picsum.photos/605/900'),
+            aw.Image(
+                imageId: 2,
+                artId: 1,
+                imageUrl: 'https://picsum.photos/600/900'),
+            aw.Image(
+                imageId: 3, artId: 1, imageUrl: 'https://picsum.photos/600/900')
+          ],
+          price: 25,
+          schoolId: 1),
+      aw.Artwork(
+          artId: 1,
+          images: [
+            aw.Image(
+                imageId: 1,
+                artId: 1,
+                imageUrl: 'https://picsum.photos/606/900'),
+            aw.Image(
+                imageId: 2,
+                artId: 1,
+                imageUrl: 'https://picsum.photos/600/900'),
+            aw.Image(
+                imageId: 3, artId: 1, imageUrl: 'https://picsum.photos/600/900')
+          ],
+          price: 25,
+          schoolId: 1),
+      aw.Artwork(
+          artId: 1,
+          images: [
+            aw.Image(
+                imageId: 1,
+                artId: 1,
+                imageUrl: 'https://picsum.photos/607/900'),
+            aw.Image(
+                imageId: 2,
+                artId: 1,
+                imageUrl: 'https://picsum.photos/600/900'),
+            aw.Image(
+                imageId: 3, artId: 1, imageUrl: 'https://picsum.photos/600/900')
+          ],
+          price: 25,
+          schoolId: 1),
+      aw.Artwork(
+          artId: 1,
+          images: [
+            aw.Image(
+                imageId: 1,
+                artId: 1,
+                imageUrl: 'https://picsum.photos/608/900'),
+            aw.Image(
+                imageId: 2,
+                artId: 1,
+                imageUrl: 'https://picsum.photos/600/900'),
+            aw.Image(
+                imageId: 3, artId: 1, imageUrl: 'https://picsum.photos/600/900')
+          ],
+          price: 25,
+          schoolId: 1),
+      aw.Artwork(
+          artId: 1,
+          images: [
+            aw.Image(
+                imageId: 1,
+                artId: 1,
+                imageUrl: 'https://picsum.photos/609/900'),
+            aw.Image(
+                imageId: 2,
+                artId: 1,
+                imageUrl: 'https://picsum.photos/600/900'),
+            aw.Image(
+                imageId: 3, artId: 1, imageUrl: 'https://picsum.photos/600/900')
+          ],
+          price: 25,
+          schoolId: 1),
+      aw.Artwork(
+          artId: 1,
+          images: [
+            aw.Image(
+                imageId: 1,
+                artId: 1,
+                imageUrl: 'https://picsum.photos/610/900'),
+            aw.Image(
+                imageId: 2,
+                artId: 1,
+                imageUrl: 'https://picsum.photos/600/900'),
+            aw.Image(
+                imageId: 3, artId: 1, imageUrl: 'https://picsum.photos/600/900')
+          ],
+          price: 25,
+          schoolId: 1),
+      aw.Artwork(
+          artId: 1,
+          images: [
+            aw.Image(
+                imageId: 1,
+                artId: 1,
+                imageUrl: 'https://picsum.photos/611/900'),
+            aw.Image(
+                imageId: 2,
+                artId: 1,
+                imageUrl: 'https://picsum.photos/600/900'),
+            aw.Image(
+                imageId: 3, artId: 1, imageUrl: 'https://picsum.photos/600/900')
+          ],
+          price: 25,
+          schoolId: 1),
+      aw.Artwork(
+          artId: 1,
+          images: [
+            aw.Image(
+                imageId: 1,
+                artId: 1,
+                imageUrl: 'https://picsum.photos/612/900'),
+            aw.Image(
+                imageId: 2,
+                artId: 1,
+                imageUrl: 'https://picsum.photos/600/900'),
+            aw.Image(
+                imageId: 3, artId: 1, imageUrl: 'https://picsum.photos/600/900')
+          ],
+          price: 25,
+          schoolId: 1),
     ];
 
     return Scaffold(
@@ -97,18 +237,19 @@ class _GalleryScreenState extends State<GalleryScreen> {
         backgroundColor: primaryColor,
         title: Text('Student Art Collection'),
       ),
-      body: buildStaggeredGridViewWithData(context, mockArtwork),);
+      body: buildStaggeredGridViewWithData(context, mockArtwork),
+    );
   }
 }
 
-Widget buildInitial(){
-  return Center(
-  );
+Widget buildInitial() {
+  return Center();
 }
 
-Widget buildStaggeredGridViewWithData(BuildContext context, List<aw.Artwork> artworkList){
-List<int> sizeList = [];
-  for(aw.Artwork artwork in artworkList){
+Widget buildStaggeredGridViewWithData(
+    BuildContext context, List<aw.Artwork> artworkList) {
+  List<int> sizeList = [];
+  for (aw.Artwork artwork in artworkList) {
     sizeList.add(randomInRange(12, 20));
   }
 
@@ -122,27 +263,15 @@ List<int> sizeList = [];
           onTap: () {
             //Navigate to carousel view
           },
-          child: Container(
-            padding: EdgeInsets.all(2.0),
-            child: Center(
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(cardCornerRadius),
-                  image: DecorationImage(
-                    image: NetworkImage(artworkList[index].images[0].imageUrl),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-            ),
+          child: StaggeredGridTile(
+            artwork: artworkList[index],
           ),
         ),
       ],
     ),
     staggeredTileBuilder: (int index) {
-      StaggeredTile staggeredTile = StaggeredTile.count(
-          staggerCount ~/ numOfRows,
-          sizeList[index]);
+      StaggeredTile staggeredTile =
+          StaggeredTile.count(staggerCount ~/ numOfRows, sizeList[index]);
       return staggeredTile;
     },
     mainAxisSpacing: 16.0,
@@ -150,3 +279,35 @@ List<int> sizeList = [];
   );
 }
 
+class StaggeredGridTile extends StatelessWidget {
+  final aw.Artwork artwork;
+
+  const StaggeredGridTile({
+    Key key,
+    this.artwork,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(2.0),
+      child: Center(
+        child: Stack(
+          children: <Widget>[
+            BuildLoading(),
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: imageBorderColor),
+                borderRadius: BorderRadius.circular(cardCornerRadius),
+                image: DecorationImage(
+                  image: NetworkImage(artwork.images[0].imageUrl),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
