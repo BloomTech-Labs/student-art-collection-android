5a6ae012623c6387a638b7725bded3ad2498143e
BrandonLively <brandonlively@live.com>
2020-01-10 17:40:57 -0500

    update secrets

--- lib/features/buy_art/presentation/bloc/gallery/gallery_bloc.dart
+++ lib/features/buy_art/presentation/bloc/gallery/gallery_bloc.dart
@@ -24,7 +24,7 @@ class GalleryBloc extends Bloc<GalleryEvent, GalleryState> {
         final artworkList = await artworkRepository.getAllArtwork();
         yield* artworkList.fold(
             (failure) async* {
-              //TODO: replace message with const
+              //TODO: replace message with cons
               yield GalleryErrorState(message: "Failed to load Artwork List");
             },
               (artworkList) async* {
