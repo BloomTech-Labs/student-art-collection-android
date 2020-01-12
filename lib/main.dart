8e01cbd6565c9b8a839e1b5aead3802db38dd7c3
BrandonLively <akutenshi@live.com>
2020-01-12 01:49:05 -0500

    test git config

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:student_art_collection/core/util/theme_constants.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'app_localization.dart';
 import 'service_locator.dart' as locator;
-
+//test
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await locator.init();
